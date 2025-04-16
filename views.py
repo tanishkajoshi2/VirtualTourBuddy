f928db9a8a23768d17c2f2f03103eacbe506e3ea
tanishkajoshi2 <tanishkajoshi130@gmail.com>
2025-04-17 01:52:14 +0530

    Delete views.py

--- views.py
+++
@@ -1,6 +0,0 @@
-from django.http import JsonResponse
-from django.shortcuts import render  # Import only necessary functions
-
-
-def flight_list(request):
-    return JsonResponse({"message": "Flight list endpoint"})
