cec3f471d84474dfe2410bb3498add5fea55d629
tanishkajoshi2 <tanishkajoshi130@gmail.com>
2025-04-17 01:51:25 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from api.models import Flight
-from api.serializers import FlightSerializer 
-
-
-class FlightViewSet(viewsets.ModelViewSet):
-    queryset = Flight.objects.all()
-    serializer_class = FlightSerializer
