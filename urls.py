c856cf6d4a275ac940db15f3aa0f35f9b422a942
tanishkajoshi2 <tanishkajoshi130@gmail.com>
2025-04-17 01:51:52 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('flights.urls')),  # Ensure 'flights' has a urls.py file
-    path("api/v1/", include('api.urls')),  # Ensure 'api' has a urls.py file
-]
